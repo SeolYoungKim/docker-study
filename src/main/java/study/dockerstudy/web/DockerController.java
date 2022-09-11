474983e412c60eba2e0ebdb3398a156b9f32a4c2
SeolYoungKim <nasur4da@gmail.com>
2022-09-11 18:29:27 +0900

    [STUDY] Docker image layer && command_22.09.11

--- src/main/java/study/dockerstudy/web/DockerController.java
+++ src/main/java/study/dockerstudy/web/DockerController.java
@@ -1,16 +1,19 @@
 package study.dockerstudy.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class DockerController {
 
     @GetMapping("/")
     public String docker() {
-        return "Hello Docker!!!!!!!!!!!";
+        log.info("THIS IS DOCKER!!!!!!");
+        return "Hello Docker!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
     }
 
     @PostMapping("/")
